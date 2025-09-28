350289cf874e52c48656791f18c7eb58e2c2b5e2
C10H12O2 <Eugene.emmanuel2007@gmail.com>
2025-09-29 00:07:05 +0300

    Added the actual days of the week to the variable  daysOfWeek

--- script.js
+++ script.js
@@ -2,3 +2,5 @@ const akanNames = {
     male:["Kwasi, Kwadwo, Kwabena, Kwaku, Yaw, Kofi, Kwame"],
     female:["Akosua, Adwoa, Abenaa, Akua, Yaa, Afua, Ama"]
 };
+
+const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
