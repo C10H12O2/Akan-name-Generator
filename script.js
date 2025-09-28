d2791933ee74af9ae03712ee03180eefdfed47f4
C10H12O2 <Eugene.emmanuel2007@gmail.com>
2025-09-29 00:19:29 +0300

    Added logic to retrieve the input values from the form and stores as reference for Akan name to be shown

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 const akanNames = {
-    male:["Kwasi, Kwadwo, Kwabena, Kwaku, Yaw, Kofi, Kwame"],
-    female:["Akosua, Adwoa, Abenaa, Akua, Yaa, Afua, Ama"]
+    male: ["Kwasi, Kwadwo, Kwabena, Kwaku, Yaw, Kofi, Kwame"],
+    female: ["Akosua, Adwoa, Abenaa, Akua, Yaa, Afua, Ama"]
 };
 
 const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+document.getElementById("Akan-Form").addEventListener("submit", function (e) {
+    e.preventDefault();
+
+const day = parseInt(document.getElementById("day").value);
+const month = parseInt(document.getElementById("month").value);
+const year = parseInt(document.getElementById("year").value);
+const gender = document.getElementById("gender").value;
+const resultDiv = document.getElementById("result");
+
+});   
